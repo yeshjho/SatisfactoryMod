f6b9fa79f63aa3f43f579290e17386832db754c9
yeshjho <yeshjho@naver.com>
2024-12-21 17:09:27 -0800

    위치 제대로 잡기 완료

--- Mods/ActualMap/Source/ActualMap/Private/ActualMapGameWorldModule.cpp
+++ Mods/ActualMap/Source/ActualMap/Private/ActualMapGameWorldModule.cpp
@@ -3,14 +3,16 @@
 #include "CanvasItem.h"
 #include "Engine/Canvas.h"
 #include "Engine/CanvasRenderTarget2D.h"
-#include "Kismet/KismetRenderingLibrary.h"
 
 #include "FGLightweightBuildableSubsystem.h"
+#include "FGBuildable.h"
+#include "FGBuildableFoundation.h"
+#include "FGSaveSession.h"
 
 #include "Patching/NativeHookManager.h"
 
 
-constexpr int RENDER_TEXTURE_SIZE = 4096;
+constexpr int RENDER_TEXTURE_SIZE = 1024 * 8;
 
 constexpr double WEST_BOUND_CENTIMETERS = -324'698.832031;
 constexpr double EAST_BOUND_CENTIMETERS = 425'301.832031;
@@ -19,40 +21,16 @@ constexpr double SOUTH_BOUND_CENTIMETERS = 375'000;
 constexpr double MAP_WIDTH_CENTIMETERS = EAST_BOUND_CENTIMETERS - WEST_BOUND_CENTIMETERS;
 constexpr double MAP_HEIGHT_CENTIMETERS = SOUTH_BOUND_CENTIMETERS - NORTH_BOUND_CENTIMETERS;
 constexpr double ORIGIN_UV[] = { -WEST_BOUND_CENTIMETERS / MAP_WIDTH_CENTIMETERS, -NORTH_BOUND_CENTIMETERS / MAP_HEIGHT_CENTIMETERS };
+constexpr double PIXEL_PER_CENTIMETER[] = { RENDER_TEXTURE_SIZE / MAP_WIDTH_CENTIMETERS, RENDER_TEXTURE_SIZE / MAP_HEIGHT_CENTIMETERS };
 
 
-UE5Coro::TCoroutine<> UActualMapGameInstanceModule::RedrawMap(AFGLightweightBuildableSubsystem* Instance, FForceLatentCoroutine)
+FVector2D world_position_to_screen_position(const FVector& WorldPosition, float Width, float Height)
 {
-	UKismetRenderingLibrary::ClearRenderTarget2D(this, RenderTarget, { 0, 0, 0, 0 });
-
-	UCanvas* Canvas = nullptr;
-	FVector2D Size;
-	FDrawToRenderTargetContext Context;
-	UKismetRenderingLibrary::BeginDrawCanvasToRenderTarget(this, RenderTarget, Canvas, Size, Context);
-
-	auto Budget = UE5Coro::Latent::FTickTimeBudget::Milliseconds(1);
-
-	for (const auto& [type, arr] : Instance->mBuildableClassToInstanceArray)
-	{
-		for (const auto& data : arr)
-		{
-			//FVector loc = data.Transform.GetLocation();
-			//FRotator rot = data.Transform.GetRotation().Rotator();
-
-			//FVector2D ScreenPosition = FVector2D(originUV[0] + loc.X / mapWorldSize, originUV[1] + loc.Y / mapWorldSize) * renderTextureSize;
-			//float Rotation = rot.Yaw;
-			//FCanvasTileItem TileItem(ScreenPosition, RenderTexture->GetResource(), { 800 * pixelPerMeter, 800 * pixelPerMeter }, { 0, 0 }, { 1, 1 }, FLinearColor::White);
-			//TileItem.Rotation = FRotator(0, Rotation, 0);
-			//TileItem.PivotPoint = { 0.5, 0.5 };
-			//TileItem.BlendMode = FCanvas::BlendToSimpleElementBlend(EBlendMode::BLEND_Translucent);
-			////TileItem.Z = loc.Z;
-			//Canvas->DrawItem(TileItem);
-
-			co_await Budget;
-		}
-	}
-
-	UKismetRenderingLibrary::EndDrawCanvasToRenderTarget(this, Context);
+	return FVector2D{
+		// TODO: Width / 2 & Height / 2: Only verified for foundations
+		ORIGIN_UV[0] + (WorldPosition.X - Width / 2) / MAP_WIDTH_CENTIMETERS,
+		ORIGIN_UV[1] + (WorldPosition.Y - Height / 2) / MAP_HEIGHT_CENTIMETERS
+	} * RENDER_TEXTURE_SIZE;
 }
 
 
@@ -66,13 +44,137 @@ void UActualMapGameInstanceModule::DispatchLifecycleEvent(ELifecyclePhase Phase)
     }
 
 
+	const auto LambdaAfterLoadGame =
+		[this](bool ReturnValue, UFGSaveSession* Instance, const FString& SaveName)
+		{
+			//for (const auto [obj, ver] : UFGSaveSession::mObjectToSerailizedVersion)
+			//{
+			//	if (!obj->IsA<AFGBuildable>())
+			//	{
+			//		continue;
+			//	}
+			//}
+
+			RedrawMap(AFGLightweightBuildableSubsystem::Get(Instance));
+		};
+
+
     const auto LambdaAfterAddFromBuildableInstanceData = 
-        [](int32 ReturnValue, AFGLightweightBuildableSubsystem* Instance, TSubclassOf< class AFGBuildable > BuildableClass, 
+        [this](int32 ReturnValue, AFGLightweightBuildableSubsystem* Instance, TSubclassOf< class AFGBuildable > BuildableClass, 
             FRuntimeBuildableInstanceData& BuildableInstanceData, bool FromSaveData = false, int32 SaveDataBuildableIndex = INDEX_NONE, 
             uint16 ConstructId = MAX_uint16, AActor* BuildEffectInstigator = nullptr, int32 BlueprintBuildEffectIndex = INDEX_NONE)
         {
-            UE_LOG(LogTemp, Warning, TEXT("Added buildable instance data"));
+            if (!FromSaveData)
+            {
+				RedrawMap(Instance);
+            }
         };
 
+
+	SUBSCRIBE_UOBJECT_METHOD_AFTER(UFGSaveSession, LoadGame, LambdaAfterLoadGame);
     SUBSCRIBE_UOBJECT_METHOD_AFTER(AFGLightweightBuildableSubsystem, AddFromBuildableInstanceData, LambdaAfterAddFromBuildableInstanceData);
+}
+
+
+void UActualMapGameInstanceModule::RedrawMap(AFGLightweightBuildableSubsystem* Instance)
+{
+	if (!Instance)
+	{
+		UE_LOG(LogTemp, Error, TEXT("ActualMap: Instance is null"));
+		return;
+	}
+
+	if (!Coroutine.IsDone())
+	{
+        Coroutine.Cancel();
+        IsPendingRedraw = true;
+        PendingBuildingData.LightweightBuildables = Instance->mBuildableClassToInstanceArray;
+	}
+	else
+	{
+        CurrentBuildingData.LightweightBuildables = Instance->mBuildableClassToInstanceArray;
+		Coroutine = RedrawMapCoroutine();
+	}
+}
+
+
+UE5Coro::TCoroutine<> UActualMapGameInstanceModule::RedrawMapCoroutine(FForceLatentCoroutine)
+{
+	ON_SCOPE_EXIT
+	{
+        OnCoroutineFinishedOrCancelled();
+	};
+
+    // Copying the buildable data would have spent some time, so yielding here.
+	co_await UE5Coro::Latent::NextTick();
+
+	UE5Coro::Latent::FTickTimeBudget Budget = UE5Coro::Latent::FTickTimeBudget::Milliseconds(0.5);
+
+	UKismetRenderingLibrary::ClearRenderTarget2D(this, RenderTarget, { 0, 0, 0, 0 });
+
+	UCanvas* Canvas = nullptr;
+	FVector2D Size;
+	UKismetRenderingLibrary::BeginDrawCanvasToRenderTarget(this, RenderTarget, Canvas, Size, RenderContext);
+
+	for (const auto& [Type, Arr] : CurrentBuildingData.LightweightBuildables)
+	{
+		float Width = 0;
+		float Height = 0;
+		if (Type->IsChildOf(AFGBuildableFoundation::StaticClass()))
+		{
+			AFGBuildableFoundation* Foundation = Cast<AFGBuildableFoundation>(Type->ClassDefaultObject);
+			Width = Foundation->mWidth;
+			Height = Foundation->mDepth;
+		}
+		else
+		{
+			continue;
+		}
+
+		const TSoftObjectPtr<UTexture2D> Texture = BuildableToIconMap.FindRef(Type.Get());
+		if (Texture.IsNull())
+		{
+			continue;
+		}
+		const UTexture2D* LoadedTexture = co_await UE5Coro::Latent::AsyncLoadObject(Texture);
+
+		for (const FRuntimeBuildableInstanceData& InstanceData : Arr)
+		{
+			const auto& Scale = InstanceData.Transform.GetScale3D();
+            Width *= Scale.X;
+            Height *= Scale.Y;
+
+			const FVector2D ScreenPosition = world_position_to_screen_position(InstanceData.Transform.GetLocation(), Width, Height);
+			FCanvasTileItem TileItem{
+				ScreenPosition,
+				LoadedTexture->GetResource(),
+				{ Width * PIXEL_PER_CENTIMETER[0], Height * PIXEL_PER_CENTIMETER[1] },
+				{ 0, 0 },
+				{ 1, 1 },
+				FLinearColor::White
+			};
+			TileItem.Rotation = InstanceData.Transform.GetRotation().Rotator();
+			TileItem.PivotPoint = { 0.5, 0.5 };
+			TileItem.BlendMode = FCanvas::BlendToSimpleElementBlend(EBlendMode::BLEND_Translucent);
+			Canvas->DrawItem(TileItem);
+
+			co_await Budget;
+		}
+	}
+}
+
+
+void UActualMapGameInstanceModule::OnCoroutineFinishedOrCancelled()
+{
+	UKismetRenderingLibrary::EndDrawCanvasToRenderTarget(this, RenderContext);
+
+	if (!IsPendingRedraw)
+	{
+		return;
+	}
+
+	// The coroutine is also on the game thread, so I think no data race here.
+    IsPendingRedraw = false;
+    CurrentBuildingData = std::move(PendingBuildingData);
+	Coroutine = RedrawMapCoroutine();
 }
--- Mods/ActualMap/Source/ActualMap/Public/ActualMapGameWorldModule.h
+++ Mods/ActualMap/Source/ActualMap/Public/ActualMapGameWorldModule.h
@@ -1,7 +1,10 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "Kismet/KismetRenderingLibrary.h"
 #include "Module/GameInstanceModule.h"
+
+#include "FGLightweightBuildableSubsystem.h"
 
 #include "UE5Coro/UE5Coro.h"
 
@@ -12,6 +15,12 @@ class UCanvasRenderTarget2D;
 
 class AFGBuildable;
 class AFGLightweightBuildableSubsystem;
+
+
+struct FBuildingData
+{
+	TMap<TSubclassOf<AFGBuildable>, TArray<FRuntimeBuildableInstanceData>> LightweightBuildables;
+};
 
 
 /**
@@ -26,7 +35,10 @@ public:
 	virtual void DispatchLifecycleEvent(ELifecyclePhase Phase) override;
 
 private:
-	UE5Coro::TCoroutine<> RedrawMap(AFGLightweightBuildableSubsystem* Instance, FForceLatentCoroutine);
+    void RedrawMap(AFGLightweightBuildableSubsystem* Instance);
+	UE5Coro::TCoroutine<> RedrawMapCoroutine(FForceLatentCoroutine = {});
+
+	void OnCoroutineFinishedOrCancelled();
 
 
 protected:
@@ -35,4 +47,10 @@ protected:
 
 	UPROPERTY(EditDefaultsOnly)
 	TObjectPtr<UCanvasRenderTarget2D> RenderTarget;
+
+	UE5Coro::TCoroutine<> Coroutine = UE5Coro::TCoroutine<>::CompletedCoroutine;
+    FDrawToRenderTargetContext RenderContext;
+	FBuildingData CurrentBuildingData;
+	bool IsPendingRedraw = false;
+	FBuildingData PendingBuildingData;
 };
