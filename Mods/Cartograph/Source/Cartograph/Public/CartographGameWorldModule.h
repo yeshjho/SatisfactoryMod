5737baffd0deb00706b5d65e23106b00f0e5826a
yeshjho <yeshjho@naver.com>
2024-12-26 19:57:04 -0800

    Fix client map not being cleared upon joining

--- Mods/Cartograph/Source/Cartograph/Public/CartographGameWorldModule.h
+++ Mods/Cartograph/Source/Cartograph/Public/CartographGameWorldModule.h
@@ -1,144 +1,18 @@
 #pragma once
 
 #include "CoreMinimal.h"
-#include "Kismet/KismetRenderingLibrary.h"
-#include "Module/GameInstanceModule.h"
-
-#include "FGLightweightBuildableSubsystem.h"
-
-#include "UE5Coro/UE5Coro.h"
-
+#include "Module/GameWorldModule.h"
 #include "CartographGameWorldModule.generated.h"
-
-
-class AFGBuildable;
-class AFGLightweightBuildableSubsystem;
-class UCanvasRenderTarget2D;
-class UFGBuildCategory;
-
-
-DECLARE_LOG_CATEGORY_EXTERN(LogCartograph, Display, All);
-
-
-struct FBuildingData
-{
-    TWeakObjectPtr<AFGBuildable> Buildable;  // nullptr for LightweightBuildables.
-    TSubclassOf<AFGBuildable> BuildableClass;
-	FTransform Transform;
-	FFactoryCustomizationData CustomizationData;
-
-	bool operator==(const FBuildingData& Other) const noexcept;
-	auto operator<=>(const FBuildingData& Other) const noexcept;
-};
-
-
-USTRUCT()
-struct FCategoryData
-{
-	GENERATED_BODY()
-
-	UPROPERTY(EditDefaultsOnly)
-	FLinearColor MainColor;
-
-	UPROPERTY(EditDefaultsOnly)
-	FLinearColor OutlineColor;
-
-	UPROPERTY(EditDefaultsOnly)
-	float OutlineThickness;
-};
-
-
-USTRUCT()
-struct FSplineData
-{
-	GENERATED_BODY()
-
-	UPROPERTY(EditDefaultsOnly)
-	FLinearColor Color;
-
-	UPROPERTY(EditDefaultsOnly)
-    float Thickness;
-
-	UPROPERTY(EditDefaultsOnly)
-	FName SparsityConfigName;
-
-	int SparsityCached;
-};
-
-
-USTRUCT()
-struct FWireData
-{
-	GENERATED_BODY()
-
-	UPROPERTY(EditDefaultsOnly)
-    FLinearColor Color;
-
-	UPROPERTY(EditDefaultsOnly)
-	float Thickness;
-};
 
 
 /**
  * 
  */
 UCLASS()
-class CARTOGRAPH_API UCartographGameInstanceModule : public UGameInstanceModule
+class CARTOGRAPH_API UCartographGameWorldModule : public UGameWorldModule
 {
 	GENERATED_BODY()
 
 public:
 	virtual void DispatchLifecycleEvent(ELifecyclePhase Phase) override;
-
-private:
-	UE5Coro::TCoroutine<> InitialBuildableGather(TArray<TWeakObjectPtr<AFGBuildable>> Factories, TMap<TSubclassOf<AFGBuildable>, TArray<FRuntimeBuildableInstanceData>> Buildings, FForceLatentCoroutine = {});
-    void RedrawMap();
-	UE5Coro::TCoroutine<> RedrawMapCoroutine(TArray<FBuildingData> AddedBuildings, TArray<FBuildingData> RemovedBuildings, FForceLatentCoroutine = {});
-
-	void OnCoroutineFinishedOrCancelled();
-
-
-protected:
-	UPROPERTY(EditDefaultsOnly)
-	TMap<TSoftClassPtr<UFGBuildCategory>, FCategoryData> BuildCategoryDataMap;
-
-	UPROPERTY(EditDefaultsOnly)
-	TMap<TSoftClassPtr<AFGBuildable>, FCategoryData> BuildableBuildCategoryDataOverrideMap;
-
-	UPROPERTY(EditDefaultsOnly)
-	TMap<TSubclassOf<UFGFactoryCustomizationDescriptor_Material>, FCategoryData> MaterialBuildCategoryDataOverrideMap;
-
-	UPROPERTY(EditDefaultsOnly)
-	TMap<TSoftClassPtr<AFGBuildable>, TSoftObjectPtr<UTexture2D>> BuildableIconOverrideMap;
-
-	UPROPERTY(EditDefaultsOnly)
-	TMap<TSoftClassPtr<AFGBuildable>, FVector2D> BuildableSizeOverrideMap;
-
-	UPROPERTY(EditDefaultsOnly)
-	TMap<TSoftClassPtr<AFGBuildable>, FRotator> BuildableExtraRotationMap;
-
-	UPROPERTY(EditDefaultsOnly)
-	TMap<TSoftClassPtr<AFGBuildable>, FSplineData> BuildableSplineDataMap;
-
-	UPROPERTY(EditDefaultsOnly)
-    TMap<TSoftClassPtr<AFGBuildable>, FWireData> BuildableWireDataMap;
-
-	UPROPERTY(EditDefaultsOnly)
-	TMap<TSoftClassPtr<AFGBuildable>, TSoftClassPtr<AFGBuildable>> BuildableClassRedirectMap;
-
-	UPROPERTY(EditDefaultsOnly)
-	TObjectPtr<UCanvasRenderTarget2D> RenderTarget;
-
-
-	bool ShouldInitialize = false;
-	UPROPERTY(BlueprintReadOnly)
-	bool IsInitializing = false;
-
-	UE5Coro::TCoroutine<> Coroutine = UE5Coro::TCoroutine<>::CompletedCoroutine;
-	FDrawToRenderTargetContext RenderContext;
-	TArray<FBuildingData> CurrentBuildingData;
-
-	bool IsPendingRedraw = false;
-	TArray<FBuildingData> PendingAddBuildingData;
-	TArray<FBuildingData> PendingRemoveBuildingData;
 };
