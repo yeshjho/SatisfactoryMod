a54df0180e96ca80c2fdc1132889ebf2438dba0a
yeshjho <yeshjho2@gmail.com>
2025-11-30 17:44:36 +0800

    Done some work needed for POC of the new method

---
+++ Mods/Cartograph/Source/Cartograph/Private/MapCaptureComponent2D.cpp
@@ -0,0 +1,7 @@
+﻿#include "MapCaptureComponent2D.h"
+
+
+const AActor* UMapCaptureComponent2D::GetViewOwner() const
+{
+	return GetOwner();
+}
---
+++ Mods/Cartograph/Source/Cartograph/Public/MapCaptureComponent2D.h
@@ -0,0 +1,18 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/SceneCaptureComponent2D.h"
+#include "MapCaptureComponent2D.generated.h"
+
+
+/**
+ * 
+ */
+UCLASS(hidecategories = (Collision, Object, Physics, SceneComponent), ClassGroup = Rendering, editinlinenew, meta = (BlueprintSpawnableComponent))
+class CARTOGRAPH_API UMapCaptureComponent2D : public USceneCaptureComponent2D
+{
+	GENERATED_BODY()
+
+public:
+	virtual const AActor* GetViewOwner() const override;
+};
